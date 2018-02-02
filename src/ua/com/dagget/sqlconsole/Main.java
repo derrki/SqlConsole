d3d4fd48f34888990290578bacf0abddfeb63092
Roman Kaminski <derrki@gmaol.com>
2018-02-03 00:54:52 +0200

    1. Created the first class Main.java

---
+++ src/ua/com/dagget/sqlconsole/Main.java
@@ -0,0 +1,10 @@
+package ua.com.dagget.sqlconsole;
+
+/**
+ * 1. Make it work. 2. Make it right. 3. Make it fast
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Helo sql console !!!");
+    }
+}
