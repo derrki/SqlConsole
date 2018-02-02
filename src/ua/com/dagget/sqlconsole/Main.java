d41ea173bd26918b5454c7db278d70e460780b70
Roman Kaminski <derrki@gmaol.com>
2018-02-03 01:45:32 +0200

    2. Added java code connect to database

--- src/ua/com/dagget/sqlconsole/Main.java
+++ src/ua/com/dagget/sqlconsole/Main.java
@@ -1,10 +1,53 @@
 package ua.com.dagget.sqlconsole;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * 1. Make it work. 2. Make it right. 3. Make it fast
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helo sql console !!!");
+        System.out.println("-------- PostgreSQL "
+                + "JDBC Connection Testing ------------");
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+
+        } catch (ClassNotFoundException e) {
+
+            System.out.println("Where is your PostgreSQL JDBC Driver? "
+                    + "Include in your library path!");
+            e.printStackTrace();
+            return;
+
+        }
+
+        System.out.println("PostgreSQL JDBC Driver Registered!");
+
+        Connection connection = null;
+
+        try {
+
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://127.0.0.1:5432/sqlcmd", "postgres",
+                    "postgres");
+
+        } catch (SQLException e) {
+
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
     }
+
 }
