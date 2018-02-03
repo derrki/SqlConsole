dd923b69fbf13045f6fa9ecb6a5e833a9d888935
Roman Kaminski <derrki@gmaol.com>
2018-02-03 02:06:40 +0200

    3. Minimisation java code connect to database

--- src/ua/com/dagget/sqlconsole/Main.java
+++ src/ua/com/dagget/sqlconsole/Main.java
@@ -8,46 +8,13 @@ import java.sql.SQLException;
  * 1. Make it work. 2. Make it right. 3. Make it fast
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("-------- PostgreSQL "
-                + "JDBC Connection Testing ------------");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        try {
 
-            Class.forName("org.postgresql.Driver");
+        Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:postgresql://127.0.0.1:5432/sqlcmd", "postgres",
+                "postgres");
 
-        } catch (ClassNotFoundException e) {
-
-            System.out.println("Where is your PostgreSQL JDBC Driver? "
-                    + "Include in your library path!");
-            e.printStackTrace();
-            return;
-
-        }
-
-        System.out.println("PostgreSQL JDBC Driver Registered!");
-
-        Connection connection = null;
-
-        try {
-
-            connection = DriverManager.getConnection(
-                    "jdbc:postgresql://127.0.0.1:5432/sqlcmd", "postgres",
-                    "postgres");
-
-        } catch (SQLException e) {
-
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return;
-
-        }
-
-        if (connection != null) {
-            System.out.println("You made it, take control your database now!");
-        } else {
-            System.out.println("Failed to make connection!");
-        }
     }
-
 }
