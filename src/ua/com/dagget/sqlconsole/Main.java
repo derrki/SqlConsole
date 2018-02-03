bdac0c83b2d61da84cbe89eacc5b7b5c78d56e4e
Roman Kaminski <derrki@gmaol.com>
2018-02-03 08:16:12 +0200

    4. Added data to database

--- src/ua/com/dagget/sqlconsole/Main.java
+++ src/ua/com/dagget/sqlconsole/Main.java
@@ -3,18 +3,25 @@ package ua.com.dagget.sqlconsole;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * 1. Make it work. 2. Make it right. 3. Make it fast
  */
 public class Main {
+
+
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
 
 
         Class.forName("org.postgresql.Driver");
         Connection connection = DriverManager.getConnection(
                 "jdbc:postgresql://127.0.0.1:5432/sqlcmd", "postgres",
                 "postgres");
+
+        Statement stmt = connection.createStatement();
+        stmt.executeUpdate("INSERT INTO public.user (id, name, password)" + "VALUES (200, 'Stiven', 'Pupkin')");
 
     }
 }
